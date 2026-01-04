5d316c6359f31f71b3d3cedae68ec08642a0a92e
shosem <shousei4794@icloud.com>
2026-01-04 17:46:01 +0900

    PostImageUploader作成、post_imageカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_27_085537) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_04_084439) do
   create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content", null: false
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_27_085537) do
 
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "post_image"
     t.text "text"
     t.string "title"
     t.datetime "updated_at", null: false
